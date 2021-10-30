4c06f317b77c21652b5907eafdfb2fd5f971df46
RejzDev <aniraze86@gmail.com>
2021-10-30 14:42:19 +0300

    new migration

---
+++ database/migrations/2021_10_30_100259_create_movies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('movie_id');
+            $table->string('name', 100);
+            $table->text('description');
+            $table->string('image', 100)->nullable();
+            $table->string('name', 300);
+            $table->string('name', 100);
+            $table->string('url', 100);
+            $table->string('title', 100);
+            $table->string('keywords', 100);
+            $table->text('seo_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
